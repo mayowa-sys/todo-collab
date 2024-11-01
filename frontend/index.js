437e8c7d6ee160d70fc73430a30a58fd2e8e32f9
Powerloop400 <mabdurrahman.balogun@gmail.com>
2024-11-01 17:15:55 +0100

    main page, create form, and general layout

---
+++ frontend/index.js
@@ -0,0 +1,34 @@
+var modal = document.getElementById("myModal");
+var btn = document.getElementById("create");
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+  modal.style.display = "block";
+  
+  setTimeout(() => {
+    modal.style.opacity = "1";
+    document.querySelector(".modal-content").style.opacity = "1";
+    document.querySelector(".modal-content").style.transform = "scale(1)";
+  }, 10);
+}
+
+span.onclick = function() {
+  modal.style.opacity = "0";
+  document.querySelector(".modal-content").style.opacity = "0";
+  document.querySelector(".modal-content").style.transform = "scale(0.9)";
+    setTimeout(() => {
+    modal.style.display = "none";
+  }, 500);
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.opacity = "0";
+    document.querySelector(".modal-content").style.opacity = "0";
+    document.querySelector(".modal-content").style.transform = "scale(0.9)";
+    
+    setTimeout(() => {
+      modal.style.display = "none";
+    }, 500);
+  }
+}
